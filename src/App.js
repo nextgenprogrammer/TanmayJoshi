e0f58fab74dc2a7ef92d8d4a84d5a827e2e121f0
nextgenprogrammer <84678682+nextgenprogrammer@users.noreply.github.com>
2024-07-31 01:19:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Tanmay Joshi's Webiste</code><br/>
+          under progress....
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
